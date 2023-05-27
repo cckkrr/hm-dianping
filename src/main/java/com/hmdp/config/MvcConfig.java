7cdca9217f7f94d0e2eb86cd856faeaa67b836be
ckr <1021004357@qq.com>
2023-05-28 01:46:31 +0800

    添加登陆校验拦截

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,27 @@
+package com.hmdp.config;
+
+import com.hmdp.utils.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author ck
+ * @create 2023-05-28  1:19
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns( //放行路径，这些路径不被拦截
+                        "/voucher/**",
+                        "/shop/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/blog/hot",
+                        "/user/code",
+                        "/user/login"
+                );
+    }
+}
---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,46 @@
+package com.hmdp.utils;
+
+import com.hmdp.entity.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author ck
+ * @create 2023-05-28  1:07
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    /**
+     * 进入controller之前进行校验
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1.获取session
+        HttpSession session = request.getSession();
+        //2.获取session中的用户
+        Object user = session.getAttribute("user");
+        //3.判断用户是否存在
+        if (user == null) {
+            //4.不存在，拦截，返回401状态码
+            response.setStatus(401);
+            return false;
+        }
+        //5.存在，保存用户信息到ThreadLocal
+        UserHolder.saveUser((User)user);
+        //6.放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //移除用户，避免内存泄漏
+        UserHolder.removeUser();
+    }
+}
